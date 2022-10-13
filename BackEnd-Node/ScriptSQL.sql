1ade3fdcdad972dd77e49b8268182ef42b570399
fausto57 <Faustomzp@gmail.com>
2022-10-13 09:03:49 -0600

    Vista Pedidos Completada

--- BackEnd-Node/ScriptSQL.sql
+++
@@ -1,9 +0,0 @@
-Create database Libreria;
-Use Libreria;
-
-CREATE TABLE Libros (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(150),
-    Descripcion VARCHAR(100),
-    Fecha DATE
-);
